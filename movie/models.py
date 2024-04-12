b72d3378faf36bb0f8ad5ef3f8520be80e7da30a
Guillermo Ozan <95210047+guilleozan@users.noreply.github.com>
2024-04-12 15:33:05 +1200

    Update models.py
    checking code

--- movie/models.py
+++ movie/models.py
@@ -24,21 +24,19 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     image = models.ImageField(upload_to='movies', default='default_image.jpg')
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=10)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2)
     status = models.CharField(choices=STATUS_CHOICES, max_length=2)
-=======
+
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=50)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=5)
     status = models.CharField(choices=STATUS_CHOICES, max_length=5)
->>>>>>> fb08546 (Changes in the following pages,)
-=======
+
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=50)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=5)
     status = models.CharField(choices=STATUS_CHOICES, max_length=5)
->>>>>>> 68e5e43 (UPDATE files)
+
     production_of_year = models.DateField()
     views_count = models.IntegerField(default=0)
     cast = models.CharField(max_length=100)
