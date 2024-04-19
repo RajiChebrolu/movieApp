b463d3097b0c3dd369ce4af048ac5a789018c0d4
Guillermo Ozan <guillermoozan@Guillermos-iMac.local>
2024-04-19 13:39:20 +1200

    last versoin

--- contact/urls.py
+++ contact/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path("contact/", ContactView.as_view(), name="contact"),
     path("success/", SuccessView.as_view(), name="success"),
 ]
+from django.urls import path
+
+from .views import SuccessView, ContactView
+
+urlpatterns = [
+    path("contact/", ContactView.as_view(), name="contact"),
+
+]
