32f7f32fc27f08fdfb214936ab39046615812bbd
Guillermo Ozan <guillermoozan@Guillermos-iMac.local>
2024-04-19 13:55:36 +1200

    updat

--- contact/forms.py
+++ contact/forms.py
@@ -6,6 +6,7 @@ class ContactForm(forms.Form):
         widget=forms.TextInput(attrs={"placeholder": "Your e-mail", "class": "form-control" })
     )
     subject = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Subject", "class": "form-control" }))
-    message = forms.CharField(
-        widget=forms.Textarea(attrs={"placeholder": "Your message", "class": "form-control" })
-    )
+    message = forms.CharField(widget=forms.Textarea(attrs={"placeholder": "Your message", "class": "form-control" }))
+from django import forms
+
+
